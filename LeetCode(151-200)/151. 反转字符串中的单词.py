edcd2b4f82ff768b6e4fdaaab28bab8c35e26ac6
userzhongyu <153486037+userzhongyu@users.noreply.github.com>
2025-01-14 14:38:39 +0800

    151题

--- LeetCode(151-200)/151. 反转字符串中的单词.py
+++ LeetCode(151-200)/151. 反转字符串中的单词.py
@@ -1,1 +1,16 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()  # 删除首尾空格
+        s = s.split()  # 分割字符串，将会删除所有长度的空格
+        s = s[:: -1]  # 翻转单词列表
+        return ' '.join(s)  # 拼接为字符串并返回
 
+
+def main():
+    s = "a good   example"
+    # s = "EPY2giL"
+    print(Solution().reverseWords(s))
+
+
+if __name__ == '__main__':
+    main()
